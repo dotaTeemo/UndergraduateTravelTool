d3257ddf386a5fa1cffb862b0dae04994c9e300a
dotaTeemo <253146908@qq.com>
2019-01-11 16:53:02 +0800

    first commit

---
+++ UnderGraduateTravel/app/src/main/java/com/scrum/nju/undergraduatetravel/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scrum.nju.undergraduatetravel.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.scrum.nju.undergraduatetravel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
