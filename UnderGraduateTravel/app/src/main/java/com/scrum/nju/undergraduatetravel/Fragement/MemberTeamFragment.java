1e98cd5b73f989ebcb7da1f5370b538aad1d0519
yangfukang <fukangyang@transwarp.io>
2019-01-16 10:53:12 +0800

    delete team fragment add capital team fragment and member team fragment

--- UnderGraduateTravel/app/src/main/java/com/scrum/nju/undergraduatetravel/Fragement/MemberTeamFragment.java
+++ UnderGraduateTravel/app/src/main/java/com/scrum/nju/undergraduatetravel/Fragement/MemberTeamFragment.java
@@ -13,12 +13,12 @@ import com.scrum.nju.undergraduatetravel.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link TeamFragment.OnFragmentInteractionListener} interface
+ * {@link MemberTeamFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link TeamFragment#newInstance} factory method to
+ * Use the {@link MemberTeamFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TeamFragment extends Fragment {
+public class MemberTeamFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class TeamFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public TeamFragment() {
+    public MemberTeamFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class TeamFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TeamFragment.
+     * @return A new instance of fragment MemberTeamFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TeamFragment newInstance(String param1, String param2) {
-        TeamFragment fragment = new TeamFragment();
+    public static MemberTeamFragment newInstance(String param1, String param2) {
+        MemberTeamFragment fragment = new MemberTeamFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class TeamFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team, container, false);
+        return inflater.inflate(R.layout.fragment_member_team, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -75,7 +75,17 @@ public class TeamFragment extends Fragment {
         }
     }
 
-   
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
     @Override
     public void onDetach() {
         super.onDetach();
