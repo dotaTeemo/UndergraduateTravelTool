7d861e710579aa4a61b3198c0825f77139a4e5d4
yangfukang <fukangyang@transwarp.io>
2019-01-21 22:11:47 +0800

    recycleview success

--- UnderGraduateTravel/app/src/main/java/com/scrum/nju/undergraduatetravel/Fragement/FindFragment.java
+++ UnderGraduateTravel/app/src/main/java/com/scrum/nju/undergraduatetravel/Fragement/FindFragment.java
@@ -1,14 +1,18 @@
 package com.scrum.nju.undergraduatetravel.Fragement;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.scrum.nju.undergraduatetravel.Adapter.ContactAdapter;
+import com.scrum.nju.undergraduatetravel.MiddleClass.LetterView;
 import com.scrum.nju.undergraduatetravel.R;
+import com.scrum.nju.undergraduatetravel.MiddleClass.DividerItemDecoration;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +27,12 @@ public class FindFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private RecyclerView contactList;
+    private String[] contactNames;
+    private LinearLayoutManager layoutManager;
+    private LetterView letterView;
+    private ContactAdapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,13 +69,35 @@ public class FindFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_find, container, false);
+        View view= inflater.inflate(R.layout.fragment_find, container, false);
+        contactNames = new String[] {"安然","奥兹","德玛","张三丰", "郭靖", "黄蓉", "黄老邪", "赵敏", "123", "天山童姥", "任我行", "于万亭", "陈家洛", "韦小宝", "$6", "穆人清", "陈圆圆", "郭芙", "郭襄", "穆念慈", "东方不败", "梅超风", "林平之", "林远图", "灭绝师太", "段誉", "鸠摩智"};
+        contactList = (RecyclerView) view.findViewById(R.id.contact_list);
+        letterView = (LetterView) view.findViewById(R.id.letter_view);
+        layoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        adapter = new ContactAdapter(getActivity(), contactNames);
+        contactList.setLayoutManager(layoutManager);
+        contactList.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+        contactList.setAdapter(adapter);
+
+        letterView.setCharacterListener(new LetterView.CharacterClickListener() {
+            @Override
+            public void clickCharacter(String character) {
+                layoutManager.scrollToPositionWithOffset(adapter.getScrollPosition(character),0);
+            }
+
+            @Override
+            public void clickArrow() {
+                layoutManager.scrollToPositionWithOffset(0,0);
+            }
+        });
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
